6b6a812ff39e353a24fe0d4c2ad7fb0b535a200a
zLuke2000 <luca100.lc@gmail.com>
2021-04-21 16:48:10 +0200

    update login and registration activity
    string.xml updated

--- app/src/main/java/it/uninsubria/talks/Registrazione.kt
+++ app/src/main/java/it/uninsubria/talks/Registrazione.kt
@@ -3,9 +3,9 @@ package it.uninsubria.talks
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 
-class MainActivity : AppCompatActivity() {
+class Registrazione : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContentView(R.layout.activity_registrazione)
     }
 }
